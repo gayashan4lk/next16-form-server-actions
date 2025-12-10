452287cbbbfc6aeb9257174402d12d2e56090684
gayashan4lk <gayashan4lk@gmail.com>
2025-12-10 22:45:27 +0530

    Implement form submission handling and enhance layout structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,44 @@
 import Image from 'next/image'
 
+async function saveAction(formData: FormData) {
+	'use server'
+	const fullName = formData.get('fullName')
+	console.log('form submitted')
+	console.log('Full Name:', fullName)
+}
+
 export default function Home() {
 	return (
 		<div>
-			<main>
-				<h1 className="text-4xl font-semibold">Next.js 16 - form - server actions</h1>
+			<main className="mx-auto max-w-3xl">
+				<h1 className="text-4xl font-semibold mb-6">Next.js 16 - form - server actions</h1>
+				<form action={saveAction}>
+					<div className="space-y-4">
+						<div className="space-y-1">
+							<label htmlFor="name" className="block font-semibold">
+								Name
+							</label>
+							<input
+								type="text"
+								name="name"
+								placeholder="Enter your name"
+								className="border-2 border-slate-300 p-2 rounded-md block"
+							/>
+						</div>
+						<div className="space-y-1">
+							<label htmlFor="email" className="block font-semibold">
+								Email
+							</label>
+							<input
+								type="text"
+								name="email"
+								placeholder="Enter your email"
+								className="border-2 border-slate-300 p-2 rounded-md block"
+							/>
+						</div>
+						<button className="bg-slate-200 font-semibold cursor-pointer p-2 rounded-md px-4">Submit</button>
+					</div>
+				</form>
 			</main>
 		</div>
 	)
