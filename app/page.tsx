a3794b5836847b8678dfaefb972e86727b10aa86
gayashan4lk <gayashan4lk@gmail.com>
2025-12-10 22:48:45 +0530

    Update button styling: change font weight to bold

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
 								className="border-2 border-slate-300 p-2 rounded-md block"
 							/>
 						</div>
-						<button type="submit" className="bg-sky-500 text-white font-semibold cursor-pointer p-2 rounded-md px-4">
+						<button type="submit" className="bg-sky-500 text-white font-bold cursor-pointer p-2 rounded-md px-4">
 							Submit
 						</button>
 					</div>
