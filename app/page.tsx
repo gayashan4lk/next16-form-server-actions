09d2b7d90d1c8f3d755b21cd28516ef40a88f699
gayashan4lk <gayashan4lk@gmail.com>
2025-12-10 21:40:56 +0530

    Refactor code style: unify quote usage and clean up layout structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <div>
-      <main>
-        <h1 className="text-4xl font-semibold">
-          Next.js 16 - form - server actions
-        </h1>
-      </main>
-    </div>
-  );
+	return (
+		<div>
+			<main>
+				<h1 className="text-4xl font-semibold">Next.js 16 - form - server actions</h1>
+			</main>
+		</div>
+	)
 }
